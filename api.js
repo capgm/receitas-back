5110034e121d0e5e164be309d35ce65af7a33d74
capgm <charles.antonio10@gmail.com>
2024-02-16 08:48:39 -0300

    ajustes e correções

--- api.js
+++ api.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const serverless = require("serverless-http");
+const bodyParser = require('body-parser');
 const router = express.Router();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV == "production") {
   mongoose.connect(
